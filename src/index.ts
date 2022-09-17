cb10fe6cef9b2292826d8f596468c27770848134
Corpus <jardel_corpus@hotmail.com>
2022-09-17 11:45:21 -0500

    08-clases-02

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,31 @@
-class tabla{
-    private numero:number;
-    tem:number;
+class operacion{
+    private a:number;
+    private b:number;
+    cont:number;
+ 
 
-    setNumero(a:number){
-        this.numero=a;
+    setNumero(a:number, b:number){
+        this.a=a;
+        this.b=b;
     }
     getNumero():number{
-        return this.numero;
+        return this.a, this.b;
+
     }
     calcular():void{
-        for(let i=1; i<11; ++i){
-            console.log(`${this.numero} x ${i} = ${i*this.numero}`)
+        for(let i=1; i<=this.b; ++i){
+            console.log(`${this.a} + ${this.b} = ${this.cont=this.b+this.b}`)
         }
-        let s:number=1;
+        /*let s:number=1;
         while(s<11){
             console.log(`${this.numero} x ${s} = ${s*this.numero}`)
             ++s;
-        }
+        }*/
     }
 }
-let tabla1 = new tabla();
-tabla1.setNumero(7);
-console.log('variable privada: ' + tabla1.getNumero());
+let operacion1 = new operacion();
+operacion1.setNumero(5,7);
+/*console.log('variable privada: ' + operacion.getNumero());
 tabla1.tem=6;
-console.log('variable publica: ' + tabla1.tem);
-tabla1.calcular();
+console.log('variable publica: ' + tabla1.tem);*/
+operacion1.calcular();
